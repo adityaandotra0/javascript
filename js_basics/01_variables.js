aa52360b0bdfad583a071c9f5ff656201d7d66a2
Aditya Andotra <98409200+adityaandotra0@users.noreply.github.com>
2023-02-22 17:33:09 +0000

    Variables

---
+++ js_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 11389
+let accountEmail = "aditya@google.com"
+var accountPassword = "12345"
+accountCity = "Kathua"
+
+//accountID = 2
+accountEmail = "11233@hp.com"
+accountPassword = "54321"
+accountCity = "Jammu"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
