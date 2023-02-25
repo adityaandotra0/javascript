cd5f1c96d4ab1210a37a20ac10c506005255585a
Aditya Andotra <98409200+adityaandotra0@users.noreply.github.com>
2023-02-25 06:49:01 +0000

    DataTypes

---
+++ js_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all JS cose as newer version
+
+console.log(2 + 3)
+console.log("Aditya")
+
+//null -> standlone value
+//symbol -> Unique
+//boolean -> true/false
+
+console.log(typeof undefined); //type - undefined
+console.log(typeof null); //type - object
